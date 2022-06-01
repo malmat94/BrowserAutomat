0021fb50659d7d2c602312f4c5d1783a6435a6e0
Mateusz laptop <malicki9mateusz4@gmail.com>
2022-06-01 22:30:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(executable_path="C:/Users/Maliccy/AppData/Local/Programs/chromedriver.exe", chrome_options = options)
